401f4ebe45d1a699bb3cfa5731061269474026d1
sairajr62 <sairajr62@gmail.com>
2025-02-16 19:52:07 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/project" element={<Project />} />
-          
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>
