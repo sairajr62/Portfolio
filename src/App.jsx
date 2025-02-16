63db7333cb1b0dded8b4ab8fcce154a4b85cd115
sairajr62 <sairajr62@gmail.com>
2025-02-16 19:53:12 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 
 function App() {
   return (
