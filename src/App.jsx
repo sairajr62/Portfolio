4a281f234e9a6a83bfaea63c17da72340a7c23dd
sairajr62 <sairajr62@gmail.com>
2025-02-25 20:58:30 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/contact";
 import Layout from "./components/Layout/Layout";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
