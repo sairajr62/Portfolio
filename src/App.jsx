139e1d6d0dd1f6eab74bc7d39d766974718c51e7
sairajr62 <sairajr62@gmail.com>
2025-02-26 11:52:34 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/contact";
 import Layout from "./components/Layout/Layout";
-import Projects from "./pages/Projects";
+/*import Projects from "./pages/Projects";*/
 
 function App() {
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="projects" element={<Projects />} />
-      </Route>
-    </Routes>
-  </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="projects" element={<Projects />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
