b9dddca4ada97e052eabbfddf8b88e40120b7210
sairajr62 <sairajr62@gmail.com>
2025-03-02 13:04:52 +0530

    commit 5

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,19 +4,20 @@ function About() {
   return (
     <>
       <div>
-        <div className="text-center mt-96">
+        <div className="text-center mt-8">
           <h2 className="font-mono font-medium">About Me</h2>
         </div>
-        <div className="w-full flex flex-row justify-center mt-16">
-          <p className="mt-8 text-center font-Garamond text-lg justify-between">
+        <div className="">
+          <p className="mt-8 text-center font-Garamond text-lg inline-block">
             Hi, I'm Sairaj Sunil Rane, a passionate web developer with a strong
-            interest in building dynamic and user-friendly websites. I
-            specialize in WordPress, JavaScript, React, and MongoDB, with
+            interest in building dynamic and user-friendly websites.
+            I specialize in WordPress, JavaScript, React, and MongoDB, with
             experience in frontend technologies like HTML, CSS, and Bootstrap. I
             enjoy exploring trending technologies and continuously improving my
-            skills. Currently, I am expanding my expertise in React, Vite, and
-            Three.js, aiming to create interactive web applications. Beyond
-            coding, I love experimenting with freelancing website development
+            skills. 
+            Currently, I am expanding my expertise in React, Vite, and
+            Three.js, aiming to create interactive web applications. 
+            Beyond coding, I love experimenting with freelancing website development
             and food order & delivery platforms. My goal is to create efficient
             and scalable digital solutions that enhance user experience. Feel
             free to check out my work on GitHub <a href="https://github.com/Sairajr62">Sairajr62</a> and connect with me
