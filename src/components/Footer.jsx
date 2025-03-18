a13d6a89c932a4791b7fa9b6fb2ebc5708239a8c
sairajr62 <sairajr62@gmail.com>
2025-03-18 23:14:25 +0530

    my 2nd footer commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <footer className="bg-gray-800 text-white p-10 text-center">
+    <footer className="bg-gray-800 text-white p-20 text-center">
         <p className='font-serif'>&copy; {new Date().getFullYear()} Designed and Developed by Sairaj Rane . All rights reserved.</p>
     </footer>
     </>
